227c9edfbcf0c2c54b30b245881b5874575b7b7e
JohnDwain <jle@johnnys-macbook-pro.home>
2020-02-13 19:36:42 +0100

    Test commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        float number0 = 10;
+        float number1 = 3;
+        float result = number0 / number1;
+        System.out.println(result);
+    }
+}
